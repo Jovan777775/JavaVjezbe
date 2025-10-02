c2fffcfb8c6dab9ac4901fa1538c0c3eff8a23a0
Jovan777775 <teyzor7@gmail.com>
2025-10-02 15:05:57 +0200

    Add files via upload

---
+++ PrviCas/src/Dvorac.java
@@ -0,0 +1,32 @@
+/*import java.util.Scanner;
+public class Dvorac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Unesite broj katapulta:");
+    int N = sc.nextInt();
+    System.out.println("Unesite maksimalnu distancu za domet:");
+    int D = sc.nextInt();
+    System.out.println("Unesite health dvorca:");
+    int HD = sc.nextInt();
+    System.out.println("Unesite napad katapulta:");
+    int KA = sc.nextInt();
+    int brKatapultaIzDometa = 0;
+    for (int i = 0; i < N; i++) {
+    	int xKatapulta = sc.nextInt();
+    	int yKatapulta = sc.nextInt();
+    	int menhetnRastojanje = Math.abs(xKatapulta) + Math.abs(yKatapulta);
+    	if (menhetnRastojanje <= D) {
+    		brKatapultaIzDometa++;
+    	}
+    }System.out.print("Dvorac je srusen");
+    if(brKatapultaIzDometa * KA >= HD) {
+    	System.out.println
+    	("Dvorac je srusen");
+    }else {
+    	System.out.println("Dvorac nije srusen");
+    	}
+    }
+	}
+*/
---
+++ PrviCas/src/Trkac.java
@@ -0,0 +1,9 @@
+
+public class Trkac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
